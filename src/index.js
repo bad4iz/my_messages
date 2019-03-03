7f215e7daae3c4b359a59e5d8227193daaeb3a6a
bad4iz <bad4iz@gmail.com>
2019-03-03 14:27:15 +0400

    video chat

--- src/index.js
+++ src/index.js
@@ -1,26 +1,49 @@
-module.hot.accept();
-
 import Peer from 'simple-peer';
+// module.hot.accept();
+const constraints = { audio: false, video: { width: 1280, height: 720 } };
 
-const peer = new Peer({
-    initiator: location.hash === '#init',
-    trickle: false,
-});
+navigator.mediaDevices.getUserMedia(constraints).then(peerFunction);
+// navigator.mediaDevices.getDisplayMedia(constraints).then(peerFunction);
 
-peer.on('signal', (data) => {
-    document.getElementById('yourId').value = JSON.stringify(data);
-});
 
-document.getElementById('connect').addEventListener('click', ()=>{
-    const otherId = JSON.parse(document.getElementById('otherId').value);
-    peer.signal(otherId);
-});
+function peerFunction(stream) {
+    const peer = new Peer({
+        initiator: location.hash === '#init',
+        trickle: false,
+        stream,
+    });
 
-document.getElementById('send').addEventListener('click', () => {
-    const yourMessage = document.getElementById('yourMessage').value;
-    peer.send(yourMessage);
-});
+    peer.on('signal', (data) => {
+        console.log('signal');
 
-peer.on('data', (data)=> {
-    document.getElementById('messages').textContent += data + '\n';
-});
+        document.getElementById('yourId').value = JSON.stringify(data);
+    });
+
+    document.getElementById('connect').addEventListener('click', ()=>{
+        console.log('connect');
+        const otherId = JSON.parse(document.getElementById('otherId').value);
+        peer.signal(otherId);
+    });
+
+    document.getElementById('send').addEventListener('click', () => {
+        const yourMessage = document.getElementById('yourMessage').value;
+        peer.send(yourMessage);
+    });
+
+    peer.on('data', (data)=> {
+        document.getElementById('messages').textContent += data + '\n';
+    });
+
+    peer.on('stream', (stream)=>{
+        const video = document.createElement('video');
+        document.body.appendChild(video);
+        video.srcObject = stream;
+        video.onloadedmetadata = function(e) {
+            video.play();
+        };
+    });
+}
+
+function error(err) {
+    console.log('The following error occurred: ' + err.name);
+}
